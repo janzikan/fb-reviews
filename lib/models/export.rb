a19a1d241f61c96080b2922780e598b07883ec49
Jan Zikan <jan.zikan@gmail.com>
2019-08-22 18:08:32 +0200

    Make Rubocop happy

--- lib/models/export.rb
+++ lib/models/export.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Export
-  COLUMNS = %i(time score author_name author_url review_url)
+  COLUMNS = %i[time score author_name author_url review_url].freeze
   SEPARATOR = "\t"
 
   class << self
